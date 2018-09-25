6b05d474a531fdf8542ba7edea851846148e8809
sowmika <sowmika148@gmail.com>
2018-09-25 12:45:04 +0530

    Sowmika | [Product Fix] Deselect the selected appointment when we move from list view to patient search

--- ui/app/appointments/controllers/manage/list/appointmentsListViewController.js
+++ ui/app/appointments/controllers/manage/list/appointmentsListViewController.js
@@ -118,6 +118,7 @@ angular.module('bahmni.appointments')
                 $stateParams.isFilterOpen = false;
                 $scope.isFilterOpen = false;
                 $stateParams.isSearchEnabled = true;
+                $scope.selectedAppointment = undefined;
             };
 
             $scope.hasNoAppointments = function () {
